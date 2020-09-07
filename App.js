41a1a8b4baed16552b28287e2c3900efe9a64f49
kristianlaw <lawkristian@hotmail.com>
2020-09-07 12:20:32 +0300

    Eka commit, tekstin vaihto

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Tähän tulee Vhattodo-botti!</Text>
       <StatusBar style="auto" />
     </View>
   );
