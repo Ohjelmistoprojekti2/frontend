9c6cca6be0bc01f63bcd480612c08d45f860f1d5
niinab <niina.blom@myy.haaga-helia.fi>
2020-09-14 12:27:36 +0300

    textinput

--- App.js
+++ App.js
@@ -3,9 +3,16 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+const [tags, setTags] = React.useState('');
+
+
   return (
     <View style={styles.container}>
       <Text>Tähän tulee Vhattodo-botti!</Text>
+      <TextInput style={{fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
+      value={tags}>
+      </TextInput>
       <StatusBar style="auto" />
     </View>
   );
