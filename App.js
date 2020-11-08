3ac9774f9a595cfe3a4cc7dc2de8bc94f45945cb
kristianlaw <37475033+kristianlaw@users.noreply.github.com>
2020-11-08 23:43:11 +0200

    Stack navigation --> Tab navigation

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { NavigationContainer} from '@react-navigation/native';
-import { createStackNavigator} from'@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Mainpage from './pages/Mainpage';
 import Activities from'./pages/Activities';
 import Fiddlin from'./pages/Playground';
+
+//npm install @react-navigation/bottom-tabs
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
 
@@ -12,11 +16,11 @@ export default function App() {
   return (
     
     <NavigationContainer>
-      <Stack.Navigator>
-      <Stack.Screen name="Mainpage" component={Mainpage} />
-        <Stack.Screen name="Activities" component={Activities} />
-        <Stack.Screen name="Playground" component={Fiddlin} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Mainpage" component={Mainpage} />
+        <Tab.Screen name="Activities" component={Activities} />
+        <Tab.Screen name="Playground" component={Fiddlin} />
+      </Tab.Navigator>
       </NavigationContainer>
    
   );
